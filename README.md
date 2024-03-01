103e765d8abfd87897d6233e7a252a7f0a452065
RajenderMunagajetti <68774396+RajenderMunagajetti@users.noreply.github.com>
2024-03-01 09:29:55 +0530

    HealthCatalyst Assignment
    HealthCatalyst Assignment

---
+++ README.md
@@ -0,0 +1,21 @@
+## Health Catalyst Automation Testing Spot Check
+
+This project contains software automation testing exercises given by the Health Catalyst over cucumber BDD requirement.
+
+## Installation
+
+```bash
+Clone Repo
+```
+
+Use the Mavin [Mvn](https://maven.apache.org/download.cgi) to install Maven.
+
+
+install the necessary dependencies:
+```bash
+Make install
+```
+Run the tests using the test runner class:
+```bash
+Make test
+```
---
+++ src/test/java/com/myProject/utilities/Driver.java
@@ -0,0 +1,89 @@
+package com.myProject.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Driver {
+
+    private Driver(){}
+
+    private static WebDriver driver;
+
+    public static WebDriver get(){
+
+        if(driver==null){
+            String browser = ConfigurationReader.get("browser");
+
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions opt = new ChromeOptions();
+
+                    // opt.setExperimentalOptions("excludeSwitches",Arrays.aslist("disable-popup-blocking"));
+    
+            // ChromeOptions opt= new ChromeOptions();
+//             //set path of .crx file of extension
+//             opt.addExtensions(new File("D:\Selenium_Cucumber_AutomationExercise\Extensioins\adblocker.crx"));
+//             //DesiredCapabilities object
+//             DesiredCapabilities c = DesiredCapabilities.chrome();
+//             // set ChromeOptions capability
+//             c.setCapability(ChromeOptions.CAPABILITY, opt);
+//             // pass capability to driver
+//             WebDriver driver = new ChromeDriver(c);
+//  WebDriver driver = new chromedriver(opt);
+ 
+                    driver = new ChromeDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+                case "ie":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "edge":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari":
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac"))
+                        throw new WebDriverException("Your OS doesn't support Safari");
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+            }
+        }
+
+        return driver;
+
+    }
+
+    public static void closeDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
